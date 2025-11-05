c599be4ab31786f24d8bf595dd986a17d6a275aa
kitsuiwebster <raphmartin1999@gmail.com>
2025-11-05 13:21:54 +0100

    feat: sea cards

--- src/app/data/seas.data.ts
+++ src/app/data/seas.data.ts
@@ -56,5 +56,141 @@ export const seasData: Card[] = [
     localisation: "Europe and Asia",
     continent: "Europe",
     depth: "2,212 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Red Sea",
+    localisation: "Africa and Asia",
+    continent: "Africa",
+    depth: "3,040 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "North Sea",
+    localisation: "Europe",
+    continent: "Europe",
+    depth: "700 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Coral Sea",
+    localisation: "Oceania",
+    continent: "Oceania",
+    depth: "9,140 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Barents Sea",
+    localisation: "Europe and Asia",
+    continent: "Europe",
+    depth: "600 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Norwegian Sea",
+    localisation: "Europe",
+    continent: "Europe",
+    depth: "3,970 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Persian Gulf",
+    localisation: "Asia",
+    continent: "Asia",
+    depth: "90 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Gulf of Mexico",
+    localisation: "North America",
+    continent: "North America",
+    depth: "4,384 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Bay of Bengal",
+    localisation: "Asia",
+    continent: "Asia",
+    depth: "4,694 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Gulf of Guinea",
+    localisation: "Africa",
+    continent: "Africa",
+    depth: "6,363 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Adriatic Sea",
+    localisation: "Europe",
+    continent: "Europe",
+    depth: "1,233 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Aegean Sea",
+    localisation: "Europe",
+    continent: "Europe",
+    depth: "3,544 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Sea of Japan",
+    localisation: "Asia",
+    continent: "Asia",
+    depth: "3,742 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Tasman Sea",
+    localisation: "Oceania",
+    continent: "Oceania",
+    depth: "5,943 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Andaman Sea",
+    localisation: "Asia",
+    continent: "Asia",
+    depth: "4,198 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "White Sea",
+    localisation: "Europe",
+    continent: "Europe",
+    depth: "340 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Irish Sea",
+    localisation: "Europe",
+    continent: "Europe",
+    depth: "175 m"
+  },
+  {
+    type: "Sea",
+    image: "assets/images/cards/default.jpg",
+    nom: "Tyrrhenian Sea",
+    localisation: "Europe",
+    continent: "Europe",
+    depth: "3,785 m"
   }
 ];
